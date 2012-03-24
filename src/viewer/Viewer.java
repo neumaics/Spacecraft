9a0af6c14e16345dc272066942c3330ef198bf8a
Mitchell Wright <pilotmitchw@gmail.com>
2012-03-23 18:46:16 -0600

    Added Builder to Viewer.java.

--- src/viewer/Viewer.java
+++ src/viewer/Viewer.java
@@ -1,33 +1,70 @@
 package viewer;
 
 public class Viewer {
-  private int WINDOW_WIDTH;
-  private int WINDOW_HEIGHT;
-  private String WINDOW_NAME;
-  private int FLOAT_BYTES = 16 * 4;
-  private float DEGTORAD = (float) (Math.PI / 180);
-  private float RADTODEG = 1 / DEGTORAD;
-  private final int WHEEL_CONST = 120;
-  private boolean HasWheel;
+  private int width;
+  private int height;
+  private String name;
   
   public static class Builder {
     //Required parameters
-    private final String WINDOW_NAME;
+    private final String name;
+    
     //Optional parameters
     private int width = 800;
     private int height = 600;
     
-    public Builder( String window_name ) {
-      WINDOW_NAME = window_name;
+    public Builder( String name ) 
+    { 
+      this.name = name; 
     }
-    
     public Builder height( int height )
-      { this.height = height; return this; }
+    { 
+      this.height = height; return this; 
+    }
     public Builder width( int width )
-      { this.width = width; return this; }
-    
-    
-    
+    { 
+      this.width = width; return this; 
+    }
+    public Viewer build()
+    {
+      return new Viewer( this );
+    }
+  }
+  
+  private Viewer( Builder builder ) {
+    this.name = builder.name;
+    this.height = builder.height;
+    this.width = builder.width;
+  }
+
+  public int getWidth()
+  {
+    return width;
+  }
+
+  public void setWidth(int width)
+  {
+    this.width = width;
+  }
+
+  public int getHeight()
+  {
+    return height;
+  }
+
+  public void setHeight(int height)
+  {
+    this.height = height;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
   }
   
 }
